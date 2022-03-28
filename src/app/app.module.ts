2306ca366db9afa9f718a6e16455f5956055f2c5
cherry-poo-manvani <poojamanvani123@gmail.com>
2022-03-28 17:56:20 +0530

    Feat: sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CoreModule,
     ReactiveFormsModule,
     HttpClientModule,
   ],
